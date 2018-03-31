199dc297046dfdea292f6e9b6a4ad60506c785c6
Jon Lauridsen <mail@jonlauridsen.com>
2018-03-31 10:45:06 +0100

    Add fileutils helper module

---
+++ tests/lib/fileutils.test.js
@@ -0,0 +1,40 @@
+const fs = require('fs-extra')
+const path = require('path')
+const tmp = require('tmp-promise')
+
+const fileutils = require('../../lib/fileutils')
+
+describe('copyDataToDir', () => {
+  let tmpPath
+
+  beforeEach(async () => {
+    const o = await tmp.dir()
+    tmpPath = o.path
+  })
+
+  afterEach(async () => fs.remove(tmpPath))
+
+  it('should create specified file', async () => {
+    await fileutils.copyDataToDir(tmpPath, {'foo.txt': new Buffer('content')})
+
+    expect(await contentsOfTmpDir()).toEqual(['foo.txt'])
+  })
+
+  it('should write content to file', async () => {
+    await fileutils.copyDataToDir(tmpPath, {'foo.txt': new Buffer('content')})
+
+    expect(await contentsOfTmpFile('foo.txt')).toEqual('content')
+  })
+
+  const contentsOfTmpFile = async filename => {
+    const fp = path.join(tmpPath, filename)
+    const buffer = await fs.readFile(fp)
+    return buffer.toString()
+  }
+
+  const contentsOfTmpDir = async () => {
+    return fs.readdir(tmpPath)
+  }
+})
+
+
