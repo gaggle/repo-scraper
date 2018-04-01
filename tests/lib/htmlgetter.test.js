5a0e1957cdb74af008893b3467eee65ae3e0f2dc
Jon Lauridsen <mail@jonlauridsen.com>
2018-04-01 18:01:34 +0100

    Introduce retrying requests on 504s
    GitHub seems to throw 504s back irregularly, so, lets try adding more layers of robustness to our request-logic?

--- tests/lib/htmlgetter.test.js
+++ tests/lib/htmlgetter.test.js
@@ -11,6 +11,16 @@ describe('HtmlGetter', () => {
   afterEach(() => nock.cleanAll())
   afterAll(() => nock.enableNetConnect())
 
+  describe('(constructor)', () => {
+    it('should refuse to construct if maxAttempts is too low', () => {
+      expect(() => new HtmlGetter({maxAttempts: 0})).toThrow()
+    })
+
+    it('should refuse to construct if retryDelay is too low', () => {
+      expect(() => new HtmlGetter({retryDelay: -1})).toThrow()
+    })
+  })
+
   describe('#makeRequest', () => {
     it('should accept a string', async () => {
       const ctx = nock('http://foo.com').get('/').reply(200)
@@ -64,6 +74,37 @@ describe('HtmlGetter', () => {
         done.fail()
       } catch (err) {
         expect(err.message).toEqual(expect.stringMatching(/ESOCKETTIMEDOUT/))
+        ctx.done()
+        done()
+      }
+    })
+
+    it('should retry a 504', async () => {
+      const ctx = nock('http://foo.com')
+        .get('/')
+        .reply(504)
+        .get('/')
+        .reply(200, 'foo')
+      htmlGetter = new HtmlGetter({maxAttempts: 2, retryDelay: 0})
+
+      const buffer = await htmlGetter.makeRequest('http://foo.com')
+
+      ctx.done()
+      expect(buffer).toBeInstanceOf(Buffer)
+      expect(buffer.toString()).toEqual('foo')
+    })
+
+    it('should fail a 504 if it goes beyond max attempts', async (done) => {
+      const ctx = nock('http://foo.com')
+        .get('/')
+        .reply(504)
+      htmlGetter = new HtmlGetter({maxAttempts: 1})
+
+      try {
+        await htmlGetter.makeRequest('http://foo.com')
+        done.fail()
+      } catch (err) {
+        expect(err.message).toEqual(expect.stringMatching(/504/))
         ctx.done()
         done()
       }
